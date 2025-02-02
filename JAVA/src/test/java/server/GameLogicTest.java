d05355070eb4b2eb80985b15f691c3bb1873ec8c
IdoMillo <idomillo@mail.tau.ac.il>
2025-02-02 11:40:34 +0200

    - Update GameLogicTest

--- JAVA/src/test/java/server/GameLogicTest.java
+++ JAVA/src/test/java/server/GameLogicTest.java
@@ -8,6 +8,18 @@ class GameLogicTest {
 
     @Test
     void testValidateGuess() {
-        assertEquals(10, gameLogic.validateGuess("10"));
+        // Guess correctness
+        String outOfBounds = "Number out of range, please guess between 1 and 100.";
+        String invalidIn = "Invalid input, please enter a number.";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> gameLogic.validateGuess("1"));
+        assertEquals(outOfBounds, exception.getMessage());
+        exception = assertThrows(IllegalArgumentException.class, () -> gameLogic.validateGuess("100"));
+        assertEquals(outOfBounds, exception.getMessage());
+        exception = assertThrows(IllegalArgumentException.class, () -> gameLogic.validateGuess("abc"));
+        assertEquals(invalidIn, exception.getMessage());
+
+        // Number generator
+        int secret = gameLogic.getSecretNumber();
+        assertTrue(gameLogic.checkGuessCorrectness(secret));
     }
 }
