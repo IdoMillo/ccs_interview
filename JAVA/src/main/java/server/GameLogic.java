05dc5798d9eef6fb4d48b63ac7bf1b289523e4e0
IdoMillo <idomillo@mail.tau.ac.il>
2025-02-02 11:01:53 +0200

    - GameLogic: generating a secret code according to the new logic

--- JAVA/src/main/java/server/GameLogic.java
+++ JAVA/src/main/java/server/GameLogic.java
@@ -3,13 +3,76 @@ package server;
 import java.util.Random;
 
 public class GameLogic {
-    private static final int SECRET_NUMBER = 42;
+    private final int PRIMES_LIMIT = 17;
     private final Random random = new Random();
+    private int secretNumber;
+
+    private boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int reverseDigits(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;  // Extract last digit
+            reversed = reversed * 10 + digit;  // Append digit to reversed number
+            number /= 10;  // Remove last digit
+        }
+        return reversed;
+    }
+
+    private int[] getPrimeArray(){
+        int numOfPrimes = countPrimes(PRIMES_LIMIT);
+        int[] primes = new int[numOfPrimes];
+        int j = 0;
+        for (int i = 0; i < PRIMES_LIMIT; i++) {
+            if (isPrime(i)) {
+                primes[j] = i;
+                j++;
+            }
+        }
+        return primes;
+    }
+
+    private void adjustToLimit(){
+        if (this.secretNumber >= 100) this.secretNumber /= 2;
+        else if (this.secretNumber < 50) this.secretNumber *= 2;
+    }
+
+    private void generateSecret() {
+        this.secretNumber = random.nextInt(101);
+        int[] primes = getPrimeArray();
+        if (secretNumber % 2 == 0){
+            this.secretNumber = reverseDigits(secretNumber);
+        } else {
+            int randomPrime = primes[random.nextInt(primes.length)];
+            this.secretNumber += randomPrime;
+        }
+        adjustToLimit();
+    }
 
     public int validateGuess(String input) throws IllegalArgumentException {
         try {
             int guess = Integer.parseInt(input);
-            if (guess < 1 || guess > 100) {
+            if (guess <= 1 || guess >= 100) {
                 throw new IllegalArgumentException("Number out of range, please guess between 1 and 100.");
             }
             return guess;
@@ -19,7 +82,7 @@ public class GameLogic {
     }
 
     public boolean checkGuessCorrectness(int guess) {
-        return guess == SECRET_NUMBER;
+        return guess == secretNumber;
     }
 
     public void generatePrefix(int guess) {
